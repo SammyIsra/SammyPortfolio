70b13ae80d728e502226a37d65f2d81f379dbdda
Sammy Israwi <sammy.israwi@gmail.com>
2017-11-13 19:10:29 -0500

    Initial boilerplate commit for the website remake

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    siteMetadata: {
+        title: "Sammy's Website"
+    },
+    plugins: [
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-styled-components"
+    ],
+};
