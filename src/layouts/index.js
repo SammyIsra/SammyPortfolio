4796ad328a739034eb6b41aac4ee10878bc37046
Sammy Israwi <sammy.israwi@gmail.com>
2018-02-25 22:41:49 -0500

    Fixed the responsiveness of the footer. Relates to #6

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,38 +5,8 @@ import Helmet from "react-helmet";
 import styled from "styled-components";
 
 import Header from "../components/Header";
+import {Footer} from "../components/Footer";
 //import "./index.css";
-
-const FooterIconsContainer = styled.footer`
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: space-evenly;
-  align-items: center;
-`;
-
-const FooterIconItem = styled.div`
-  margin-left: 1rem;
-`;
-
-const FooterContainer = styled.div`
-  background-color: ${props => props.dark ? "rgb(51,51,51)" : "white" };
-  color: ${props => props.dark? "white" : "inherit"};
-  padding: 1.5rem;
-`;
-
-const Footer = ({dark}) => 
-  <FooterContainer dark={dark}>
-    Made proudly by Sammy: Finding your lack of sense of pride and accomplishment, disturbing.
-    <FooterIconsContainer>
-      <FooterIconItem><a href="https://twitter.com/SammyIs_Me">Twitter</a></FooterIconItem>      
-      <FooterIconItem><a href="https://www.flickr.com/photos/sammy_iy/">Flickr</a></FooterIconItem>
-      <FooterIconItem><a href="https://www.linkedin.com/in/sammyisrawi/">LinkedIn</a></FooterIconItem>
-      <FooterIconItem><a href="https://dev.to/sammyisa">Dev.to</a></FooterIconItem>
-      <FooterIconItem><a href="https://github.com/SammyIsra">Github</a></FooterIconItem>
-      <FooterIconItem><a href="https://drive.google.com/file/d/0B1ouby1hW-sjSDdZSFYtUFB1TVE/view?usp=sharing">Resume</a></FooterIconItem>
-    </FooterIconsContainer>
-  </FooterContainer>;
-  
 
 class TemplateWrapper extends React.Component {
   
@@ -54,7 +24,7 @@ class TemplateWrapper extends React.Component {
     if(path === "/photographer"){
       this.setState({useDarkTheme: true});
     } else if(path === "/developer") {
-      this.setState({useDarkTheme: false});      
+      this.setState({useDarkTheme: false});
     } else {
       this.setState({useDarkTheme: false});
     }
