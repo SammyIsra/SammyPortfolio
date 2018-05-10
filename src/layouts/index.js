b13543204fdb29a8edb4d0c6b7b32beab8a4ff57
SammyIsra <sammy.israwi@gmail.com>
2018-05-09 23:43:18 -0400

    Always remember to make sure IT RUNS

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -35,20 +35,6 @@ const WebsiteContent = styled.div`
   flex: 1;
 `;
 
-const Footer = ({dark}) => 
-  <FooterContainer dark={dark}>
-    Made proudly by Sammy: Finding your lack of sense of pride and accomplishment, disturbing.
-    <FooterIconsContainer>
-      <FooterIconItem><a href="https://twitter.com/SammyIs_Me">Twitter</a></FooterIconItem>      
-      <FooterIconItem><a href="https://www.flickr.com/photos/sammy_iy/">Flickr</a></FooterIconItem>
-      <FooterIconItem><a href="https://www.linkedin.com/in/sammyisrawi/">LinkedIn</a></FooterIconItem>
-      <FooterIconItem><a href="https://dev.to/sammyisa">Dev.to</a></FooterIconItem>
-      <FooterIconItem><a href="https://github.com/SammyIsra">Github</a></FooterIconItem>
-      <FooterIconItem><a href="https://drive.google.com/file/d/0B1ouby1hW-sjSDdZSFYtUFB1TVE/view?usp=sharing">Resume</a></FooterIconItem>
-    </FooterIconsContainer>
-  </FooterContainer>;
-  
-
 class TemplateWrapper extends React.Component {
   
   componentWillMount(){
